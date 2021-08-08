6aaf48e139359150a81d32d2f0911b7af725c17c
omercalik <30555343+omercalik@users.noreply.github.com>
2021-08-08 19:22:03 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+require("isomorphic-unfetch");
+const { promises: fs } = require("fs");
+const path = require("path");
+
+async function main() {
+    const readmeTemplate = (
+        await fs.readFile(path.join(process.cwd(), "./README.template.md"))
+    ).toString("utf-8");
+}
+main();
+
+const starwars_quote = await (
+    await fetch("http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote")).json()
+
+const readme = readmeTemplate
+        .replace("{office_quote}", office_quote.data.content)
+       
+
+await fs.writeFile("README.md", readme);
